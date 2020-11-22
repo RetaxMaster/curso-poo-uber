0860c1bf2a9f5211e7de5c21ea6557085d0c1e34
RetaxMaster <retaxmaster@hotmail.com>
2020-11-22 01:06:40 -0600

    Adicion de instancias para Java y Python

--- Java/Main.java
+++ Java/Main.java
@@ -3,6 +3,11 @@ class Main {
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        car.printDataCar();
 
     }
 
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,8 @@
+from car import Car;
+
 if __name__ == "__main__":
     print("Hola Mundo")
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
+    print(vars(car))

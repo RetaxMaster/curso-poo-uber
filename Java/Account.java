e19586c63fc34bf26c8a7d5ce7add7d358e2f04a
RetaxMaster <retaxmaster@hotmail.com>
2020-11-21 21:11:30 -0600

    Adicion de las clases en Java Y Python

---
+++ Java/Account.java
@@ -0,0 +1,9 @@
+public class Account {
+
+    Integer id;
+    String name;
+    String document;
+    String email;
+    String password;
+    
+}
---
+++ Java/Route.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class Route {
+
+    Integer id;
+    ArrayList<Double> start;
+    ArrayList<Double> end;
+    
+}

aeea33065acb80b75ffa0390640106161aae249f
RetaxMaster <retaxmaster@hotmail.com>
2020-11-22 02:07:42 -0600

    Ultimas lineas de codigo:'D

--- Java/Account.java
+++ Java/Account.java
@@ -5,5 +5,10 @@ public class Account {
     String document;
     String email;
     String password;
+
+    public Account(String name, String document) {
+        this.name = name;
+        this.document = document;
+    }
     
 }
--- Java/Main.java
+++ Java/Main.java
@@ -3,9 +3,7 @@ class Main {
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
         car.passenger = 4;
         car.printDataCar();
 
